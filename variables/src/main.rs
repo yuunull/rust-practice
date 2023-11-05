a225c528ae09272ff7a96c675221790c8ad04816
yuunull <yuunull2525@gmail.com>
2023-11-05 22:40:56 +0900

    [update]function complete

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,23 @@
 fn main() {
-    let mut x = 5;
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
     println!("The value of x is: {x}");
-    x = 6;
+    another_function(100);
+    let x = plus_one(100);
     println!("The value of x is: {x}");
+}
+
+fn another_function(x: i32) {
+    println!("The value of x is: {x}");
+}
+
+fn plus_one(x: i32) -> i32 {
+    return x + 1
 }
