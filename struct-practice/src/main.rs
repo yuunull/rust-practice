70a696b949dad63accc22edbbd2b3e466dcae88e
yuunull <yuunull2525@gmail.com>
2023-12-02 10:23:19 +0900

    [update]

--- struct-practice/src/main.rs
+++ struct-practice/src/main.rs
@@ -1,18 +1,20 @@
 #[derive(Debug)]
-struct User {
-    username: String,
-    email: String,
-    sign_in_count: u64,
-    active: bool,
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
 }
 
 fn main() {
-    let user1 = User {
-        email: String::from("someone@example.com"),
-        username: String::from("someusername123"),
-        active: true,
-        sign_in_count: 1,
-    };
+    let rect1 = Rectangle { width: 30, height: 50 };
 
-    println!("The value of user1.username is: {:?}", user1);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
 }
