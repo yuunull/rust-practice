8b9904671df14dbcc5698aab995e953641bed90a
yuunull <yuunull2525@gmail.com>
2023-12-02 10:30:46 +0900

    [update]

--- struct-practice/src/main.rs
+++ struct-practice/src/main.rs
@@ -5,10 +5,18 @@ struct Rectangle {
 }
 
 impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
+}
+
+impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+}
 
+impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
@@ -17,9 +25,10 @@ impl Rectangle {
 fn main() {
     let rect1 = Rectangle { width: 30, height: 50 };
     let rect2 = Rectangle { width: 10, height: 40 };
-    let rect3 = Rectangle { width: 60, height: 45 };
+    let sq = Rectangle::square(3);
 
     // rect1にrect2ははまり込む？
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
-    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", sq.area());
 }
